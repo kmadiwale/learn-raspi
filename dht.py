f02a26154c775986d5046963a2419d28d4b5fa74
Kedar Madiwale <Kedar_Madiwale@bmc.com>
2017-09-09 12:52:07 -0500

    date range and related validation

--- dht.py
+++ dht.py
@@ -1,6 +1,17 @@
 from flask import Flask, request, render_template
+import time
+import datetime
+
 app = Flask(__name__)
 app.debug = True # Make this False if you are no longer debugging
+
+
+def validateDate(d):
+    try:
+        datetime.datetime.strptime(d, '%Y-%m-%d %H:%M')
+        return True
+    except ValueError:
+        return False
 
 @app.route("/")
 def hello():
@@ -18,16 +29,44 @@ def lab_temp():
 
 @app.route("/data")
 def read_data():
+
         import sqlite3
         conn=sqlite3.connect('/home/pi/kedar/rpi3b/labs/dht.db')
         curs=conn.cursor()
-        curs.execute("SELECT * FROM temperatures")
+        
+	curs.execute("SELECT * FROM temperatures")
         temperatures = curs.fetchall()
         curs.execute("SELECT * FROM humidities")
-        humidities = curs.fetchall()
+	humidities = curs.fetchall()
         conn.close()
+	
         return render_template("sdata.html",temp=temperatures,hum=humidities)
 
+
+@app.route("/databydates", methods=['GET'])
+def read_data_by_dates():
+
+	fromDateStr = request.args.get('f',time.strftime("%Y-%m-%d %H:%M")) #Get the from date value from the URL
+	toDateStr = request.args.get('t',time.strftime("%Y-%m-%d %H:%M"))   #Get the to date value from the URL
+
+
+	if not validateDate(fromDateStr):    # Validate date before sending it to the DB
+                fromDateStr = time.strftime("%Y-%m-%d 00:00")
+        if not validateDate(toDateStr):
+                toDateStr = time.strftime("%Y-%m-%d %H:%M")               
+
+
+        import sqlite3
+        conn=sqlite3.connect('/home/pi/kedar/rpi3b/labs/dht.db')
+        curs=conn.cursor()
+        
+	curs.execute("SELECT * FROM temperatures WHERE read_datetime BETWEEN ? AND ?", (fromDateStr, toDateStr))
+        temperatures = curs.fetchall()
+	curs.execute("SELECT * FROM humidities WHERE read_datetime BETWEEN ? AND ?", (fromDateStr, toDateStr))
+	humidities = curs.fetchall()
+        conn.close()
+	
+        return render_template("sdata.html",temp=temperatures,hum=humidities)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8282)
