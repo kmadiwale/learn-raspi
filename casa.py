bfce42028fb679d942f4143923578dff3f1c58da
Kedar Madiwale <Kedar_Madiwale@bmc.com>
2017-09-30 10:53:00 -0500

    program improvements, argument based notification

--- casa.py
+++ casa.py
@@ -19,37 +19,6 @@ pir=14
 buzz=15
 dts='%Y-%m-%d.%H-%M-%S'
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pir, GPIO.IN)
-GPIO.setup(buzz, GPIO.OUT)
-GPIO.output(buzz, GPIO.LOW)
-
-time.sleep(5) #setting up the sensor
-print ("sensor is ready!")
-
-config = configparser.ConfigParser()
-config.read('/home/pi/rpi3b/hs7/casa.ini')
-while True:
-	x=GPIO.input(pir)
-	if x==1:
-		GPIO.output(buzz, GPIO.HIGH)
-		captureTime = datetime.datetime.now().strftime(dts)
-		fname = 'IMG_intrusion_' + captureTime + '.jpg'
-		click(imgPath + fname, "PIR1")
-		GPIO.output(buzz, GPIO.LOW)
-		save("PIR1", imgPath + fname)
-		notify(config.get('mail','se_id'), 
-			config.get('mail','re_id'), 
-			config.get('mail', 'se_sub'),
-			config.get('mail', 'se_pro'),
-			config.get('mail', 'se_pro_port'),
-			config.get('mail', 'se_phrase'),
-			"PIR1", imgPath + fname, captureTime)
-
-GPIO.cleanup()
-print("resources cleaned up")
-
-
 def save(pir_id, img2Save):
 	print ("saving data, please wait...")
 	conn=sqlite3.connect('/home/pi/rpi3b/hs7/intruder.db')  
@@ -57,11 +26,11 @@ def save(pir_id, img2Save):
         curs.execute("""INSERT INTO intruder values(datetime(CURRENT_TIMESTAMP, 'localtime'),(?), (?))""", (pir_id, img2Save))
         conn.commit()
         conn.close()
-	print ("intrusion logged")
+	print ("movement logged")
 
 
 def click(f, pir_id):
-	print("Intrusion detected by " + pir_id + ", capturing image: " + f)
+	print("movement detected by " + pir_id + ", capturing image, hope you smiled: " + f)
 	os.system('raspistill -o ' + f + ' --nopreview --exposure sports --timeout 1')
 
 
@@ -89,4 +58,40 @@ def notify(from_id, email_id, subject, smtp, port, phrase, pir_id, img, capTime)
 	server.sendmail(from_id, email_id, text)
 	server.quit()
 	print("Please check your email for intruder attachment")
+
+#program begin
+if (len(sys.argv) > 1):
+	away = isinstance(sys.argv[1], bool)
+	print("away, notification:true")
+else:
+	away = False
+	print("not away, notification:false")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pir, GPIO.IN)
+GPIO.setup(buzz, GPIO.OUT)
+GPIO.output(buzz, GPIO.LOW)
+
+time.sleep(60) #setting up the sensor
+print ("sensor is ready!")
+
+config = configparser.ConfigParser()
+config.read('/home/pi/rpi3b/hs7/casa.ini')
+
+while True:
+	x=GPIO.input(pir)
+	if x==1:
+		GPIO.output(buzz, GPIO.HIGH)
+		captureTime = datetime.datetime.now().strftime(dts)
+		fname = 'IMG_intrusion_' + captureTime + '.jpg'
+		click(imgPath + fname, "PIR1")
+		GPIO.output(buzz, GPIO.LOW)
+		save("PIR1", imgPath + fname)
+		if (away):
+			notify(config.get('mail','se_id'), config.get('mail','re_id'), config.get('mail', 'se_sub'), 
+			config.get('mail', 'se_pro'),config.get('mail', 'se_pro_port'), config.get('mail', 'se_phrase'), 
+			"PIR1", imgPath + fname, captureTime)
+
+GPIO.cleanup()
+print("resources cleaned up")
 
